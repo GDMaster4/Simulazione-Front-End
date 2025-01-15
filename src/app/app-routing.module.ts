ae5082968b069e75cc67cd994e131a16e4c9f67a
GDMaster4 <dalmasogianluca@gmail.com>
2025-01-15 11:28:36 +0100

    tolta roba vecchia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { offerFiltersResolver } from './resolvers/offer-filters.resolver';
-import { OffersResolver } from './resolvers/offers.resolver';
 
 const routes: Routes = [
   {
     path:"",
-    redirectTo:"/offers?maxOfferte=5",
+    redirectTo:"/login",
     pathMatch:"full"
-  },
-  {
-    path:"offers",
-    component:HomeComponent,
-    resolve:{
-      filters:offerFiltersResolver,
-      offers:OffersResolver
-    },
-    runGuardsAndResolvers:"paramsOrQueryParamsChange"
   }
 ];
 
--- src/app/resolvers/offer-filters.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { ResolveFn } from '@angular/router';
-import { OfferFilters } from '../services/offer.service';
-
-
-export const offerFiltersResolver: ResolveFn<OfferFilters> = (route) => {
-  return route.queryParams as OfferFilters;
-};
