9fa8afa9132abf1d8b73ddde06eea27c1b7add2b
GDMaster4 <dalmasogianluca@gmail.com>
2025-01-18 10:46:33 +0100

    deploy back-end

--- collegamento.ts
+++ collegamento.ts
@@ -1,4 +1,4 @@
 export const enviroment={
-    // "apiUrl":"https://simulazione-back-end.onrender.com"
-    "apiUrl":"http://localhost:3000/api"
+    "apiUrl":"https://simulazione-back-end.onrender.com"
+    // "apiUrl":"http://localhost:3000/api"
 }
