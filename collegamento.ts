7bb265517773383463463cfeb5782308dfddb8f5
GDMaster4 <dalmasogianluca@gmail.com>
2025-01-18 10:57:32 +0100

    xd

--- collegamento.ts
+++ collegamento.ts
@@ -1,4 +1,4 @@
 export const enviroment={
-    "apiUrl":"https://simulazione-back-end.onrender.com"
+    "apiUrl":"https://simulazione-back-end.onrender.com/api"
     // "apiUrl":"http://localhost:3000/api"
 }
